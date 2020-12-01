b49ae1515cf93b59cf1e201691bee51a0b2af3fd
Shamp <blog.shamp@gmail.com>
2020-12-01 23:06:55 +0900

    style: 기본 테스트 리액트 앱 틀 및 라이브러리 추가

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const App: React.FC = () => {
     return (
-        <div className="App">Hello TypeScript Component!!</div>
+        <ContentWrapper>
+            <ContentHeader>
+                <div className="cssans__word">
+                    <b className="cssans:C"></b>
+                </div>
+            </ContentHeader>
+        </ContentWrapper>
     );
 }
+
+const ContentWrapper = styled.div`
+    background-color: rgb(97, 109, 179);
+    min-height: 100vh;
+`;
+
+const ContentHeader = styled.div`
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    box-shadow: 0 1px 0 rgba(255, 255, 255, 0.1);
+    height: 69px;
+    max-width: 1140px;
+    margin: 0 auto;
+`;
 
 export default App;
