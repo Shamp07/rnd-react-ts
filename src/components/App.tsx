1ca42de17b10cd29aa07640737f3f39a342298e8
Shamp <blog.shamp@gmail.com>
2020-12-02 23:01:02 +0900

    style: Header CsSans 도입 및 레이아웃 수정

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
+import './cssans.min.css';
 
 const App: React.FC = () => {
     return (
         <ContentWrapper>
             <ContentHeader>
-                <div className="cssans__word">
-                    <b className="cssans:C"></b>
-                </div>
+                <CssSans className="cssans__word">
+                    <b className="cssans:P" />
+                    <b className="cssans:r" />
+                    <b className="cssans:a" />
+                    <b className="cssans:c" />
+                    <b className="cssans:t" />
+                    <b className="cssans:i" />
+                    <b className="cssans:c" />
+                    <b className="cssans:e" />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <b className="cssans:A" />
+                    <b className="cssans:p" />
+                    <b className="cssans:p" />
+                </CssSans>
             </ContentHeader>
+            <ContentHeading>
+                <h1>
+                    <em>12.1</em>
+                    &nbsp;
+                    TypeScript - <strong>Intro &amp; Install</strong>
+                </h1>
+                <h2>TypeScript의 소개와 개발 환경 구축</h2>
+            </ContentHeading>
         </ContentWrapper>
     );
 }
@@ -24,6 +44,47 @@ const ContentHeader = styled.div`
     height: 69px;
     max-width: 1140px;
     margin: 0 auto;
+    font-size: 10px;
+    position: relative;
 `;
+
+const ContentHeading = styled.div`
+    padding: 120px;
+    max-width: 900px;
+    margin: 0 auto;
+    color: #f4f1ce;
+    line-height: 1.2;
+    text-align: center;
+    text-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
+    
+    & h1 {
+      font-size: 28px !important;
+      font-weight: 600 !important;
+      font-family: 'Montserrat', serif !important;
+      margin: 0;
+    }
+  
+    & h1 em {
+      font-style: normal !important;
+    }
+  
+
+    & h2 {
+      font-size: 48px !important;
+      font-weight: 300 !important;
+      font-style: normal;
+      margin: 20px 0px;
+    }
+`;
+
+const CssSans = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 10px;
+  font-size: 10px;
+  letter-spacing: -1.5px;
+  transform: translate3D(0, -50%, 0);
+`;
+
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,9 @@ module.exports = {
         test: /\.tsx?$/,
         loader: "ts-loader",
         exclude: /node_modules/
+      }, {
+        test:/\.css$/,
+        use:['style-loader','css-loader'],
       }
     ]
   },
