5a844fb3014921efc4a3f2e537db7f896ddf1bba
Shamp <blog.shamp@gmail.com>
2020-12-03 21:40:35 +0900

    style: 메인 로고 및 개발 프레임 개발 완료

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,66 +15,69 @@ const App: React.FC = () => {
                     <b className="cssans:i" />
                     <b className="cssans:c" />
                     <b className="cssans:e" />
-                    &nbsp;&nbsp;&nbsp;&nbsp;
-                    <b className="cssans:A" />
-                    <b className="cssans:p" />
-                    <b className="cssans:p" />
+                    <b className="cssans:W" />
+                    <b className="cssans:e" />
+                    <b className="cssans:b" />
                 </CssSans>
             </ContentHeader>
             <ContentHeading>
                 <h1>
-                    <em>12.1</em>
-                    &nbsp;
-                    TypeScript - <strong>Intro &amp; Install</strong>
+                    1. TypeScript - <b>Practice</b>
                 </h1>
-                <h2>TypeScript의 소개와 개발 환경 구축</h2>
+                <h2>TypeScript의 연습과 개발 환경 구축</h2>
             </ContentHeading>
+            <MainContentContainer>
+                <MainContentArticle>
+                    안녕하세요
+                </MainContentArticle>
+            </MainContentContainer>
         </ContentWrapper>
     );
 }
 
 const ContentWrapper = styled.div`
-    background-color: rgb(97, 109, 179);
-    min-height: 100vh;
+  background-color: #616db3;
+  background-image: linear-gradient(142deg, #6188b3, #617bb3 31%, #616db3 65%, #6361b3);
+  min-height: 100vh;
 `;
 
 const ContentHeader = styled.div`
-    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-    box-shadow: 0 1px 0 rgba(255, 255, 255, 0.1);
-    height: 69px;
-    max-width: 1140px;
-    margin: 0 auto;
-    font-size: 10px;
-    position: relative;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  box-shadow: 0 1px 0 rgba(255, 255, 255, 0.1);
+  height: 69px;
+  max-width: 1140px;
+  margin: 0 auto;
+  font-size: 10px;
+  position: relative;
 `;
 
 const ContentHeading = styled.div`
-    padding: 120px;
-    max-width: 900px;
-    margin: 0 auto;
-    color: #f4f1ce;
-    line-height: 1.2;
-    text-align: center;
-    text-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
-    
-    & h1 {
-      font-size: 28px !important;
-      font-weight: 600 !important;
-      font-family: 'Montserrat', serif !important;
-      margin: 0;
-    }
-  
-    & h1 em {
-      font-style: normal !important;
-    }
-  
+  padding: 120px;
+  max-width: 900px;
+  margin: 0 auto;
+  color: #f4f1ce;
+  line-height: 1.2;
+  text-align: center;
+  text-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
 
-    & h2 {
-      font-size: 48px !important;
-      font-weight: 300 !important;
-      font-style: normal;
-      margin: 20px 0px;
-    }
+  & h1 {
+    font-size: 28px !important;
+    font-weight: 400 !important;
+    font-family: "Montserrat", serif !important;
+    margin: 0;
+  }
+
+  & h1 b {
+    font-weight: 500 !important;
+    font-style: normal !important;
+  }
+
+  & h2 {
+    font-size: 48px !important;
+    font-weight: 300 !important;
+    font-style: normal;
+    margin: 20px 0px;
+  }
 `;
 
 const CssSans = styled.div`
@@ -84,6 +87,19 @@ const CssSans = styled.div`
   font-size: 10px;
   letter-spacing: -1.5px;
   transform: translate3D(0, -50%, 0);
+`;
+
+const MainContentContainer = styled.div`
+  max-width: 1140px;
+  margin: 0 auto;  
+`;
+
+const MainContentArticle = styled.article`
+  font-size: 1rem;
+  padding: 60px;
+  min-height: 100vh;
+  border-radius: 3px 3px 0 0;
+  background-color: #f4f1ce;
 `;
 
 
