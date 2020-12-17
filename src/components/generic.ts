2fe1714284c9afc8247ad9bde950d87341ab0601
Shamp <blog.shamp@gmail.com>
2020-12-18 01:14:37 +0900

    test: generic-2

--- src/components/generic.ts
+++ src/components/generic.ts
@@ -10,11 +10,37 @@ export class genericStudy {
     // queue 의 data 프로퍼티 배열에는 어떤 타입이든 가질 수 있는 배열이다.
     // 하지만 두번째 값은 '1'이라는 문자열이 들어갔고, number 타입이 아니기 때문에 두번째 toFixed 에서 에러가 발생한다.
     // =====
+    // class Queue {
+    //   protected data: any[] = [];
+    //
+    //   push(item: any) {
+    //
+    //   }
+    //
+    //   pop() {
+    //     return this.data.shift();
+    //   }
+    // }
+    //
+    // const queue = new Queue();
+
+    // queue.push(0);
+    // queue.push('1');
+    //
+    // console.log(queue.pop().toFixed());
+    // console.log(queue.pop().toFixed()); // Error
+    // =====
+
+    // =====
+    // 위와 같은 경우를 해결하기 위해 Queue 클래스를 상속하여 number 타입 전용 NumberQueue 클래스를 선언할 수 있다.
+    // 아래의 예제 같은경우는 63라인의 부분에서 에러가 발생하기 때문에 사전에 에러를 방지할 수 있다.
+    //
+    // =====
     class Queue {
       protected data: any[] = [];
 
       push(item: any) {
-
+        this.data.push(item);
       }
 
       pop() {
@@ -22,17 +48,23 @@ export class genericStudy {
       }
     }
 
-    const queue = new Queue();
+    class NumberQueue extends Queue {
+      push(item: number) {
+        super.push(item);
+      }
 
-    queue.push(0);
-    queue.push('1');
+      pop(): number {
+        return super.pop();
+      }
+    }
 
-    console.log(queue.pop().toFixed());
-    // console.log(queue.pop().toFixed()); // Error
-    // =====
+    const queue = new NumberQueue();
 
-    // =====
-    // =====
+    // queue.push('1'); // Error
+    queue.push(+'1');
+
+    console.log(queue.pop().toFixed()); // 0
+    console.log(queue.pop().toFixed()); // 1
     // =====
 
     // =====
